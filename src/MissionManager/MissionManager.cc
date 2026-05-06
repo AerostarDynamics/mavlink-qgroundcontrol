ac0781b10ba81a9cd9b81d3ab81a99bfc05aeb1c
David Howie <DaveHowie@github.com>
2026-05-05 21:09:29 -0400

    Add support for SET_CAMERA_FOCUS mission item.

--- src/MissionManager/MissionManager.cc
+++ src/MissionManager/MissionManager.cc
@@ -150,7 +150,8 @@ void MissionManager::generateResumeMission(int resumeIndex)
                            << MAV_CMD_VIDEO_START_CAPTURE
                            << MAV_CMD_VIDEO_STOP_CAPTURE
                            << MAV_CMD_DO_CHANGE_SPEED
-                           << MAV_CMD_SET_CAMERA_MODE;
+                           << MAV_CMD_SET_CAMERA_MODE
+                           << MAV_CMD_SET_CAMERA_FOCUS;
 
     bool addHomePosition = _vehicle->firmwarePlugin()->sendHomePositionToVehicle();
 
