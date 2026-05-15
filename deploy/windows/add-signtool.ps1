fcd3338a29710a36c9eeac31b11115290d4b6aea
David Howie <DaveHowie@github.com>
2026-05-15 11:08:24 -0400

    Add AeroStar required capabilities.

---
+++ deploy/windows/add-signtool.ps1
@@ -0,0 +1,35 @@
+<##
+#   Locate Windows signtool.exe and update the Windows
+#   PATH variable to include the path to signtool.
+#>
+
+Write-Output "::group::powershell $($PSCommandPath)"
+
+try {
+    $Arch = ($env:RUNNER_ARCH).ToLower()
+    $SearchBase = "${env:ProgramFiles(x86)}\Windows Kits\10\bin"
+
+    "Searching `"$SearchBase`" for signtool ($Arch)..."
+    $Tool = Get-ChildItem $SearchBase -Recurse -Force -ErrorAction SilentlyContinue |
+        Where-Object {$_.Name -eq 'signtool.exe' -and $_.Directory -like "*\$Arch"} |
+        Sort-Object -Descending |
+        Select-Object -First 1
+
+    if (!($Tool)) {throw [System.IO.FileNotFoundException]::new('File not found.', 'signtool.exe')}
+
+    $Tool.Directory.FullName | Out-File $env:GITHUB_PATH -Append
+    "signtool-$Arch=$($Tool.FullName)" | Out-File $env:GITHUB_OUTPUT -Append
+    $env:PATH = "$($Tool.Directory.FullName);$env:PATH"
+    "Added " + $Tool.Directory.FullName + " to $" + "env:GITHUB_PATH and $" + "env:PATH"
+
+    signtool.exe /h
+} catch {
+    Write-Output "::error::$($_.Exception)"
+    Write-Output '::endgroup::'
+
+    exit 1
+}
+
+Write-Output '::endgroup::'
+
+exit 0
